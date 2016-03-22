ad47d99d6e3201fecd3c207d7dbc33b212445733
dmodica3 <gdmodica@wpi.edu>
2016-03-21 20:01:25 -0400

    Added testAirports.java and testFlights.java files for specific test funtions for those files.

---
+++ FlightReservationSystem/junitTesting/testAirports.java
@@ -0,0 +1,17 @@
+/**
+ * This class is designed to handle JUnit tests for the Airports class
+ */
+
+import org.junit.Test;
+import org.junit.Assert.*;
+/**
+ * @author David
+ *
+ */
+public class testAirports {
+
+	@Test
+	public void testDoNothing(){
+		assert(true);
+	}
+}
---
+++ FlightReservationSystem/junitTesting/testFlights.java
@@ -0,0 +1,17 @@
+/**
+ * This class is designed to handle JUnit tests for the Flights class
+ */
+
+import org.junit.Test;
+import org.junit.Assert.*;
+/**
+ * @author David
+ *
+ */
+public class testFlights {
+
+	@Test
+	public void testDoNothing(){
+		assert(true);
+	}
+}
