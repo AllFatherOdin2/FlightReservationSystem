c9049368f3a622f4d291ed9c623af7fb82d81458
dmodica3 <gdmodica@wpi.edu>
2016-03-29 20:51:47 -0400

    Added attributes/constructors to Airplane.java and added DOM functions to AirplaneManager.java

--- FlightReservationSystem/src/CS509/client/airplane/Airplane.java
+++ FlightReservationSystem/src/CS509/client/airplane/Airplane.java
@@ -32,4 +32,5 @@ public class Airplane {
 		mFirstClassSeats = fcSeats;
 		mCoachSeats = cSeats;
 	}
+	
 }
--- FlightReservationSystem/src/CS509/client/airplane/AirplaneManager.java
+++ FlightReservationSystem/src/CS509/client/airplane/AirplaneManager.java
@@ -3,10 +3,91 @@
  */
 package CS509.client.airplane;
 
+import java.io.ByteArrayInputStream;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import CS509.client.flight.Flight;
+
 /**
  * @author David
  *
  */
 public class AirplaneManager {
+	HashMap<String,Airplane> airplanes; 
+	
+	/**
+	 * Basic Constructor
+	 */
+	public AirplaneManager(){
+		airplanes = new HashMap<String,Airplane>();
+	}
+	
+	/**
+	 * Constructor to set airplanes
+	 * Primarily for testing
+	 */
+	public AirplaneManager(Map<String,Airplane> map){
+		airplanes = (HashMap<String, Airplane>) map;
+	}
+	
+	public boolean addAll (String xmlPlanes) {
+		
+		boolean collectionUpdated = false;
+		
+		// Load the XML string into a DOM tree for ease of processing
+		// then iterate over all nodes adding each flight to our collection
+		Document docPlanes = buildDomDoc (xmlPlanes);
+		NodeList nodesPlanes = docPlanes.getElementsByTagName("Airplane");
+		
+		for (int i = 0; i < nodesPlanes.getLength(); i++) {
+			Element elementPlane = (Element) nodesPlanes.item(i);
+			Airplane flight = buildPlane (elementPlane);
+			
+			/*if (flight.isValid()) {
+				this.add(flight);
+				collectionUpdated = true;
+			}*/
+		}
+		
+		return collectionUpdated;
+	}
+	
+	/**
+	 * Builds a DOM tree from an XML string
+	 * Parses the XML file and returns a DOM tree that can be processed
+	 * @param xmlPlanes XML string containing set of planes
+	 * @return DOM tree from parsed XML or null if exception is caught
+	 */
+	private Document buildDomDoc(String xmlPlanes) {
+		try{
+			DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+			return docBuilder.parse(new ByteArrayInputStream(xmlPlanes.getBytes()));
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
+
+	private Airplane buildPlane(Element elementPlane) {
+		/**
+		 * Airplane will be instantiated after attributes are parsed
+		 */
+		Airplane airplane = null;
+		return null;
+	}
+
+	
+	
+	
 }
