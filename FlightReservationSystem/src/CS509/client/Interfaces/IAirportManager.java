c5245e6a750144b8ac183d52b3942880ed3482b9
dmodica3 <gdmodica@wpi.edu>
2016-04-07 14:26:20 -0400

    Updated AirportManager to act as a wrapper class for a HashMap<String,IAirport>, and updated IAirportManager to include an exception that can be thrown.

--- FlightReservationSystem/src/CS509/client/Interfaces/IAirportManager.java
+++ FlightReservationSystem/src/CS509/client/Interfaces/IAirportManager.java
@@ -1,9 +1,10 @@
 package CS509.client.Interfaces;
 
 import CS509.client.airport.Airport;
+import CS509.client.airport.AirportNotFoundException;
 
 public interface IAirportManager 
 {
-	public IAirport getAirport(String airportCode);
+	public IAirport getAirport(String airportCode) throws AirportNotFoundException;
 
 }
