305cf5ae750ae96ac80db173ed255320325708db
maggieqinlk <maggieqinlk@gmail.com>
2016-04-11 22:03:21 -0400

    Finish the FlightSort

--- FlightReservationSystem/src/CS509/client/trip/FlightSort.java
+++ FlightReservationSystem/src/CS509/client/trip/FlightSort.java
@@ -1,33 +1,48 @@
+package CS509.client.trip;
+
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
-import java.util.regex.PatternSyntaxException;
+import java.util.HashMap;
 
 import CS509.client.Interfaces.IFlight;
+import CS509.client.util.Converter;
 
 public class FlightSort{
-	@SuppressWarnings({ "unchecked", "rawtypes" })
 	SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-	@SuppressWarnings("unchecked")
-	public FlightSort(ArrayList flightList, String PriceOrTime) {
+	public FlightSort(HashMap<String, IFlight> flightMap, String PriceOrTime) {
+		ArrayList<IFlight> flightList = Converter.convertMapToArray(flightMap);
+		
         if(PriceOrTime == "P"){
 		    Comparator<IFlight> comparatorPrice = new Comparator<IFlight>(){
 			    public int compare(IFlight f1, IFlight f2){
-			    	if(f1.getmPriceCoach() != f2.getmPriceCoach()){
 			    		return f1.getmPriceCoach().compareTo(f2.getmPriceCoach());
-			    	}
 			    }
 		    };
 		    Collections.sort(flightList, comparatorPrice);
 		}
-            else{
+        
+        else{
 		    Comparator<IFlight> comparatorTime = new Comparator<IFlight>(){
 			    public int compare(IFlight f1, IFlight f2){
 			    	
-			    	Date d1 = sdf.parse(f1.getmFlightTime().substring(11));
-			    	Date d2 = sdf.parse(f2.getmFlightTime().substring(11));
+			    	Date d1 = null;
+					try {
+						d1 = sdf.parse(f1.getmFlightTime().substring(11));
+					} catch (ParseException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+			    	Date d2 = null;
+					try {
+						d2 = sdf.parse(f2.getmFlightTime().substring(11));
+					} catch (ParseException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 			    	if(d1 == null && d2 == null) return 0;
 			    	if(d1 == null) return -1;
 			    	if(d2 == null) return 1;
